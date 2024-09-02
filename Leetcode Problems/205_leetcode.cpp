dead5059c1bf547ef57155b0086de3bd7336e1d3
Sahil saini <52703428+recker624@users.noreply.github.com>
2024-09-02 18:52:17 +0530

    added additional map in problem 205 to account for bidirectional mapping of string characters

--- Leetcode Problems/205_leetcode.cpp
+++ Leetcode Problems/205_leetcode.cpp
@@ -20,17 +20,24 @@ bool isIsomorphic(string s, string t)
     return false;
   }
 
-  unordered_map<char, char> map;
+  unordered_map<char, char> map1;
+  unordered_map<char, char> map2;
   for (int i = 0; i < s.length(); i++)
   {
-    if (map.find(s[i]) != map.end())
+    if (map1.find(s[i]) != map1.end())
     {
-      if (map[s[i]] != t[i])
+      if (map1[s[i]] != t[i])
+        return false;
+    }
+    else if (map2.find(t[i]) != map2.end())
+    {
+      if (map2[t[i]] != s[i])
         return false;
     }
     else
     {
-      map[s[i]] = t[i];
+      map1[s[i]] = t[i];
+      map2[t[i]] = s[i];
     }
   }
 
